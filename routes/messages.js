e7cef848c319e14fd20ca362322858b7260bdbbc
Jon <55158803+Jlopez0307@users.noreply.github.com>
2023-03-17 09:33:21 -0400

    Finished messagely back-end

---
+++ routes/messages.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const ExpressError = require('../expressError');
+const User =  require('../models/user');
+const router = new express.Router();
+const Message = require('../models/message');
+const { ensureCorrectUser, ensureLoggedIn } = require("../middleware/auth");
+
+/** GET /:id - get detail of message.
+ *
+ * => {message: {id,
+ *               body,
+ *               sent_at,
+ *               read_at,
+ *               from_user: {username, first_name, last_name, phone},
+ *               to_user: {username, first_name, last_name, phone}}
+ *
+ * Make sure that the currently-logged-in users is either the to or from user.
+ *
+ **/
+router.get('/:id', ensureCorrectUser, async (req,res,next) => {
+    try {
+        return res.json({msg: await Message.get(req.params.id)})
+    } catch (error) {
+        return next(error);
+    }
+})
+
+/** POST / - post message.
+ *
+ * {to_username, body} =>
+ *   {message: {id, from_username, to_username, body, sent_at}}
+ *
+ **/
+router.post('/', ensureLoggedIn, async (req,res,next) => {
+    try {
+        const { from_username, to_username, body } = req.body; 
+        return res.json({message: await Message.create(from_username, to_username, body)});
+    } catch (error) {
+        return next(error);
+    }
+})
+
+/** POST/:id/read - mark message as read:
+ *
+ *  => {message: {id, read_at}}
+ *
+ * Make sure that the only the intended recipient can mark as read.
+ *
+ **/
+router.post('/:id/read', ensureCorrectUser, async (req,res,next) => {
+    try {
+        const read = await Message.markRead(req.params.id);
+        return res.json({message: read})
+    } catch (error) {
+        return next(error);
+    }
+})
+
+
+module.exports = router;
